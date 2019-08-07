f07eaef30541d6f185a694616424e12c4290c381
Geraldo Luiz <geraldorj-2010@hotmail.com>
2019-08-07 19:12:59 -0300

    Grid Layout initial commit

--- app/src/main/java/com/atelievirtual/escolavirtual/MainActivity.java
+++ app/src/main/java/com/atelievirtual/escolavirtual/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.atelievirtual.escolavirtual.R;
 
@@ -18,15 +19,55 @@ MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen);
+        setContentView(R.layout.main_screen_grid);
 
         Log.i("Print","Entered MainScreen");
 
 
-        Button gotoList = (Button) findViewById(R.id.allStudentsListButton);
+        /*Button gotoList = (Button) findViewById(R.id.allStudentsListButton);
         Button createProfile = (Button) findViewById(R.id.createProfileButton);
         Button gotoRoomList = (Button) findViewById(R.id.allRoomsListButton);
         Button createNewRoom = (Button) findViewById(R.id.createRoomButton);
+
+        gotoList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showStudentsList = new Intent(MainActivity.this, StudentListActivity.class);
+                //showStudentsList.putExtra("WhichList?","Students");
+                startActivity(showStudentsList);
+            }
+        });
+
+        createProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Print", "Goto NewDetail Clicked");
+                startActivity(new Intent(MainActivity.this, StudentDetail.class));
+            }
+        });
+
+        gotoRoomList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showRoomList = new Intent(MainActivity.this, RoomListActivity.class);
+                //showRoomList.putExtra("WhichList?","Room");
+                startActivity(showRoomList);
+            }
+        });
+
+        createNewRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showRoomDetail = new Intent(MainActivity.this, RoomDetail.class);
+                startActivity(showRoomDetail);
+            }
+        });
+        */
+
+        CardView gotoList = (CardView) findViewById(R.id.allStudentsCardView);
+        CardView createProfile = (CardView) findViewById(R.id.createStudentsCardView);
+        CardView gotoRoomList = (CardView) findViewById(R.id.allRoomsCardView);
+        CardView createNewRoom = (CardView) findViewById(R.id.createRoomCardView);
 
         gotoList.setOnClickListener(new View.OnClickListener() {
             @Override
